e25df9b7d5749e90c44d23d11b41a54cb15bd694
faizm11 <alansharimuhammadfaiz@gmail.com>
2022-08-01 20:04:05 +0800

    belum jadi

---
+++ jawaban2.py
@@ -0,0 +1,13 @@
+from xml.dom.minidom import Element
+
+from numpy import append
+
+
+data1 = [2, 4, 6, 3, 9]
+data2 = [1, 3, 5, 7, 8, 9]
+yang_sama = []
+
+if data1== data2:
+    yang_sama.insert(data1 and data2)
+    print(yang_sama)
+
